e40d0a7cd50117045ef072d67e07b88d09299b41
Fynn <0xfynnix@gmail.com>
2025-06-20 15:51:55 +0800

    fix:use force theme

--- main/app/providers.tsx
+++ main/app/providers.tsx
@@ -13,6 +13,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
+        forcedTheme="dark"
         // enableSystem
         disableTransitionOnChange
       >
