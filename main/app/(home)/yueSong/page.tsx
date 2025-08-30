eabe05e9d0a64a6df94313090f5696af8807742e
aw <19972938+Bystander2@users.noreply.github.com>
2025-08-30 21:24:25 +0800

    feat: search audio

--- main/app/(home)/yueSong/page.tsx
+++ main/app/(home)/yueSong/page.tsx
@@ -4,125 +4,100 @@ import { Card } from "@/components/ui/card";
 import {
   ChevronLeft,
   CircleArrowOutUpRight,
-  Save,
-  HeartPlus,
   Play,
   FastForward,
   VolumeX,
   Pause,
   Volume2,
+  Heart,
+  Star,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import AudioVisualizer from "./_components/audio-visualizer";
-import { Slider } from "@/components/ui/slider";
+import { Slider } from "@/components/ui/slider2";
+import {
+  corpusInteractApi,
+  IStats,
+  IUpdateInteractProps,
+} from "@/lib/api/corpus-interact";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { SearchResult } from "@/lib/api/search";
 import Lyrics from "./_components/lyrics";
+import { cn } from "@/lib/utils";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useAuthStore } from "@/lib/store/useAuthStore";
+import { toast } from "sonner";
 
 function YueSong() {
   const router = useRouter();
+  const { isAuthenticated } = useAuthStore();
+  const yueSongDetails = sessionStorage.getItem("yueSongDetails");
+  const data: SearchResult = JSON.parse(yueSongDetails);
   const audioRef = useRef<HTMLAudioElement>(null);
   const audio = audioRef.current;
   const [loaded, setLoaded] = useState(false);
-  console.log(loaded);
 
   const [isPlaying, setIsPlaying] = useState(false);
-  const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState(50);
   const [progress, setProgress] = useState(0);
   const [isSeeking, setIsSeeking] = useState(false);
+  const queryClient = useQueryClient();
 
-  const data = {
-    lyric: "今天我, 寒夜里看雪飘过",
-    song_info: {
-      context: {
-        title: "海阔天空",
-        artist: "Beyond",
-        lyric_author: "xxx",
-        tune_author: "yyy",
-        album: "乐与怒",
-        description: "Beyond 乐队的代表作，传递自由精神。",
-        duration: 258,
-      },
-      contributor: "0x05",
+  const { data: corpusStats, isLoading } = useQuery<IStats>({
+    queryKey: ["corpus-stats", data.unique_id],
+    queryFn: async () => {
+      const res = await corpusInteractApi.getStats(data.unique_id);
+      return res;
     },
-    listener_num: 10,
-    liked_num: 10,
-    collected_num: 20,
-    yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-    audio: "/test.mp3",
-    lyric_full: [
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 5,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 10,
-      },
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 15,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 20,
-      },
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 25,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 30,
-      },
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 35,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 40,
-      },
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 45,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 50,
-      },
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 55,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 60,
-      },
-      {
-        yue_pin: "gam1 tin1 ngo5, hon4 je6 leoi5 hon3 syut3 piu1 gwo3",
-        lyric: "今天我, 寒夜里看雪飘过",
-        time: 65,
-      },
-      {
-        yue_pin: "xx yy zz, dd tt yy uu ii ii oo",
-        lyric: "123, 456789",
-        time: 70,
-      },
-    ],
-  };
+  });
+
+  const mutation = useMutation({
+    mutationFn: async ({
+      corpus_unique_id,
+      is_liked,
+      is_bookmarked,
+    }: IUpdateInteractProps) => {
+      return await corpusInteractApi.updateInteract({
+        corpus_unique_id,
+        is_liked,
+        is_bookmarked,
+      });
+    },
+    onSuccess: (result) => {
+      queryClient.invalidateQueries({
+        queryKey: ["corpus-stats", data.unique_id],
+      });
+    },
+  });
+  const lyrics = useMemo(() => {
+    const lyricsOriginal = data.note.context.lyric;
+    const duration = lyricsOriginal.reduce((acc, cur) => {
+      acc += cur.sec;
+      return acc;
+    }, 0);
+    const lyricsCurrent = lyricsOriginal.reduce((acc, cur) => {
+      if (!acc.length) {
+        acc.push({ ...cur, start: 0 });
+      } else {
+        const newCur = {
+          ...cur,
+          start: acc[acc.length - 1].sec + acc[acc.length - 1].start,
+        };
+        acc.push(newCur);
+      }
+      return acc;
+    }, []);
+    return {
+      duration,
+      lyric_full: lyricsCurrent,
+    };
+  }, [data.note.context.lyric]);
 
   const togglePlay = () => {
     if (!audio) return;
@@ -134,10 +109,11 @@ function YueSong() {
       setIsPlaying(false);
     }
   };
-  const toggleMute = () => {
+
+  const handleVolumeChange = (val: number) => {
     if (!audio) return;
-    audio.muted = !audio.muted;
-    setIsMuted(audio.muted);
+    audio.volume = val / 100;
+    setVolume(val);
   };
   const handleSeek = (val) => {
     setProgress(Number(val));
@@ -174,6 +150,38 @@ function YueSong() {
       if (interval) clearInterval(interval);
     };
   }, [isSeeking, audio]);
+
+  function debounce(fn: Function, delay = 500) {
+    let timer: NodeJS.Timeout;
+    return function (...args: any[]) {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => fn(...args), delay);
+    };
+  }
+
+  const handleProtectedClick = useCallback(
+    debounce((name: string) => {
+      mutation.mutate({
+        corpus_unique_id: data.unique_id,
+        ...(name === "like"
+          ? { is_liked: true }
+          : name === "dislike"
+            ? { is_liked: false }
+            : {}),
+        ...(name === "bookmark"
+          ? { is_bookmarked: true }
+          : name === "disbookmark"
+            ? { is_bookmarked: false }
+            : {}),
+      });
+    }, 500),
+    []
+  );
+
+  const handleClick = (name: string) => {
+    isAuthenticated ? handleProtectedClick(name) : toast("Please Login");
+  };
+
   // useEffect(() => {
   //   console.log("1:", audio);
   //   if (!audio) return;
@@ -190,21 +198,30 @@ function YueSong() {
   //   };
   // }, [audio]);
 
-  console.log("current:", audio?.currentTime, progress);
+  useEffect(() => {
+    if (progress >= lyrics.duration) {
+      setIsPlaying(false);
+      setProgress(0);
+    }
+  }, [progress, lyrics.duration]);
+
+  // console.log("current:", audio?.currentTime, progress, lyrics.duration);
   return (
     <Card className="container mx-10 p-6 space-y-8 flex my-8 overflow-y-scroll shadow-md ">
       <div className="flex justify-between text-gray-600 dark:text-gray-400 mb-2">
-        <div className="flex cursor-pointer" onClick={() => router.back()}>
+        <div className="flex cursor-pointer" onClick={() => router.push("/")}>
           <ChevronLeft />
           <span className="pl-2">Go Back</span>
         </div>
-        <div className="text-sm">
-          {Number(data.listener_num + 1).toLocaleString()} listens
-        </div>
+        {!isLoading ? (
+          <div className="text-sm">
+            {Number(corpusStats?.stats.views).toLocaleString()} listens
+          </div>
+        ) : null}
       </div>
       <div className="flex flex-row flex-wrap">
-        <div className="flex-[1] justify-items-center -ml-10">
-          <div className="lg:w-56 lg:h-56 overflow-hidden rounded-lg w-34 h-34">
+        <div className="lg:flex-[1] justify-items-center -ml-10 flex-[2]">
+          <div className="lg:w-56 lg:h-56 overflow-hidden rounded-lg w-30 h-30">
             <Image
               src="/album_cover.png"
               alt="cover"
@@ -218,7 +235,7 @@ function YueSong() {
           <AudioVisualizer isPlaying={isPlaying} />
           <Slider
             value={[progress]}
-            max={data.song_info.context.duration}
+            max={lyrics.duration}
             step={1}
             className="w-[60%]"
             onValueChange={handleSeek}
@@ -233,11 +250,23 @@ function YueSong() {
                 <Pause onClick={togglePlay} />
               )}
               <FastForward onClick={speedUp} />
-              {isMuted ? (
-                <VolumeX onClick={toggleMute} />
-              ) : (
-                <Volume2 onClick={toggleMute} />
-              )}
+              <Popover>
+                <PopoverTrigger asChild>
+                  {volume === 0 ? <VolumeX /> : <Volume2 />}
+                </PopoverTrigger>
+                <PopoverContent className="w-40">
+                  <div className="flex flex-col items-center space-y-2">
+                    <span className="text-sm">Volume: {volume}%</span>
+                    <Slider
+                      value={[volume]}
+                      min={0}
+                      max={100}
+                      step={1}
+                      onValueChange={(val) => handleVolumeChange(val[0])}
+                    />
+                  </div>
+                </PopoverContent>
+              </Popover>
             </div>
           ) : (
             <div className="my-4">Loading...</div>
@@ -245,7 +274,7 @@ function YueSong() {
           <audio
             ref={audioRef}
             controls
-            src={data.audio}
+            src={data.note.context.audio}
             hidden
             preload="auto"
             onLoadedMetadata={() => {
@@ -254,23 +283,62 @@ function YueSong() {
             }}
           />
         </div>
-        <div className="flex-[2] text-white">
+        <div className="lg:flex-[2] text-white">
           <div className="flex justify-between">
             <div className="text-2xl font-bold">
-              {data.song_info.context.title}
+              {data.note.context.song_name}
             </div>
             <div className="flex gap-4">
-              <HeartPlus />
-              <Save />
-              <CircleArrowOutUpRight />
+              {!isAuthenticated || !corpusStats?.user_status?.is_liked ? (
+                <Heart
+                  className="cursor-pointer"
+                  onClick={() => handleClick("like")}
+                />
+              ) : (
+                <Heart
+                  className="cursor-pointer text-red-400 fill-red-400"
+                  onClick={() => handleClick("dislike")}
+                />
+              )}
+              {!isAuthenticated || !corpusStats?.user_status?.is_bookmarked ? (
+                <Star
+                  className="cursor-pointer"
+                  onClick={() => handleClick("bookmark")}
+                />
+              ) : (
+                <Star
+                  className="cursor-pointer text-yellow-400 fill-yellow-400"
+                  onClick={() => handleClick("disbookmark")}
+                />
+              )}
+              <a
+                href={`https://card.app.aidimsum.com//?uuid=${data.unique_id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={(e: any) => {
+                  const url = e.currentTarget.href;
+                  navigator.clipboard.writeText(url).then(() => {
+                    toast("Link copied.");
+                  });
+                }}
+              >
+                <CircleArrowOutUpRight className="cursor-pointer" />
+              </a>
             </div>
           </div>
           <div className="my-4">
-            <p>词：{data.song_info.context.lyric_author}</p>
-            <p>曲：{data.song_info.context.tune_author}</p>
+            <p>
+              词：
+              {/* {data.note.context.lyric_author} */}
+            </p>
+            <p>
+              曲：
+              {/* {data.note.context.tune_author} */}
+            </p>
           </div>
           <Lyrics
             data={data}
+            lyric={lyrics}
             progress={progress}
             handleSeekEnd={handleSeekEnd}
           />
